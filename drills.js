ef5b19867416d22123077dd30c1ce8166501efe2
laratarpley13 <ltarp13@gmail.com>
2021-04-09 11:05:36 -0500

    drills

---
+++ drills.js
@@ -0,0 +1,32 @@
+/*
+Understanding merge sort
+    21, 1, 26, 45, 29, 28, 2, 9, *split* 16, 49, 39, 27, 43, 34, 46, 40
+*/
+
+/*
+Understanding quicksort
+*/
+
+/*
+Implementing quicksort
+*/
+
+/*
+Implementing merge sort
+*/
+
+/*
+Sorting a linked list using merge sort
+*/
+
+/*
+Bucket sort
+*/
+
+/*
+Sort in Place
+*/
+
+/*
+Sorting books
+*/
